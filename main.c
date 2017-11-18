bfc81af5c41e62cd1842cbe0743028339580da95
Fedor Chukharev <randomfedor@gmail.com>
2017-11-19 01:29:40 +0300

    Add straight move
    Signed-off-by: Fedor Chukharev <randomfedor@gmail.com>

--- main.c
+++ main.c
@@ -37,12 +37,81 @@ void __print_matrix(double **matrix, size_t side) {
     return;
 }
 
+double fabs(double number) {
+    return number * (number > 0 ? 1 : -1);
+}
+
+size_t find_fitting_row(double **matrix, size_t side, size_t column) {
+    size_t j, res = (matrix[column][column] == 0 ? (size_t)-1 : column);
+#ifdef _MAIN_ELEM
+    double max_val = fabs(matrix[column][column]);
+#endif
+    for (j = column; j < side; j++) {
+#ifdef _MAIN_ELEM
+        if (fabs(matrix[j][column]) > max_val) {
+            res = j;
+        }
+#endif
+#ifndef _MAIN_ELEM
+        if (matrix[j][column]) {
+            return j;
+        }
+#endif
+    }
+    return res;
+}
+
+int substract_line_from_line(double **matrix, size_t side, size_t from, size_t which) {
+    size_t j, first_elem = (size_t)-1;
+    for (j = 0; j < side; j++) {
+        if (matrix[which][j]) {
+            first_elem = j;
+            break;
+        }
+    }
+    if (first_elem == (size_t)-1) {
+        return -1;
+    }
+    double modifier = matrix[from][first_elem] / matrix[which][first_elem];
+    for (j = first_elem+1; j < side+1; j++) {
+        if (from != which) {
+            matrix[from][j] -= modifier * matrix[which][j];
+        }
+        matrix[which][j] /= matrix[which][first_elem];
+    }
+    if (from != which) {
+        matrix[from][first_elem] = 0;
+    }
+    matrix[which][first_elem] = 1;
+    return 0;
+}
+
+int straigt_move(double **matrix, size_t side) {
+    size_t column, row, s_row;
+    for (column = 0; column < side; column++) {
+        if ((s_row = find_fitting_row(matrix, side, column)) == (size_t)-1) {
+            printf("No non-zero element in column, exiting...\n");
+            return 1;
+        }
+        for (row = column; row < side; row++) {
+            if ((substract_line_from_line(matrix, side, row, s_row) == -1)) {
+                printf("No non-zero element in column, exiting...\n");
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
 int main() {
     size_t side;
     printf("Enter n - side of matrix:\n");
     scanf("%zu", &side);
     double **matrix = get_matrix(side);
     fill_matrix(matrix, side);
+    __print_matrix(matrix, side);
+    straigt_move(matrix, side);
+    printf("#%-10s#\n", "");
     __print_matrix(matrix, side);
     return 0;
 }
